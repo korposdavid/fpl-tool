5a636acec07052aacdcb84c2f27df71471a0146a
David Korpos <korposdavid@gmail.com>
2020-02-06 13:21:45 +0100

    init StarterTeamContainer

---
+++ src/components/StarterTeamContainer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const StarterTeamContainer = () => {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col sm={8}></Col>
+                    <Col sm={4}></Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default StarterTeamContainer
