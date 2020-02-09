8c8a2f2396bd7b3e131e1608397d994ee07851ac
David Korpos <korposdavid@gmail.com>
2020-02-09 21:39:22 +0100

    add grey bg color

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./layout/NavBar"
 import StarterTeamContainer from "./components/StarterTeamContainer";
+import "./App.css";
+
 
 const App = () => {
   return (
