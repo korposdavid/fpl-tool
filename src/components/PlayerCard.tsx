313991102f61afbc4d83147af4183bdd446f3790
David Korpos <korposdavid@gmail.com>
2020-02-12 16:19:05 +0100

    use Badge component for all data points on playerCard

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -16,36 +16,26 @@ const PlayerCard: React.FC<Props> = props => {
     return status === "a" ? "success" : status === "i" ? "danger" : "warning";
   };
 
+  const playerName =
+    props.player.first_name.slice(0, 1) +
+    ". " +
+    props.player.second_name +
+    " (" +
+    props.player.teamObject.short_name +
+    ")";
+
   return (
     <Card bg={cardStatus()} text="white">
       <Card.Body>
-        <Container>
-          <Row>
-            <Col sm={9}>
-              {" "}
-              <Card.Title>
-                {props.player.first_name.slice(0, 1) +
-                  ". " +
-                  props.player.second_name +
-                  " (" +
-                  props.player.teamObject.short_name +
-                  ")"}
-              </Card.Title>
-              <hr />
-              <Card.Text>
-                <Badge variant="primary">ICT: {props.player.ict_index}</Badge>
-                <Badge variant="secondary">I: {props.player.influence}</Badge>
-                <Badge variant="secondary">C: {props.player.creativity}</Badge>
-                <Badge variant="secondary">T: {props.player.threat}</Badge>
-              </Card.Text>
-            </Col>
-            <Col sm={3}>
-              <Card.Text>Price: {props.player.now_cost / 10.0}</Card.Text>
-              <hr />
-              <Card.Text>Form: {props.player.form}</Card.Text>
-            </Col>
-          </Row>
-        </Container>
+        <Card.Title>{playerName}</Card.Title>
+        <Card.Text>
+          <Badge variant="primary">ICT: {props.player.ict_index}</Badge>
+          <Badge variant="secondary">I: {props.player.influence}</Badge>
+          <Badge variant="secondary">C: {props.player.creativity}</Badge>
+          <Badge variant="secondary">T: {props.player.threat}</Badge>
+          <Badge variant="secondary">Price: {props.player.now_cost / 10.0}</Badge>
+          <Badge variant="secondary">Form: {props.player.form}</Badge>
+        </Card.Text>
       </Card.Body>
     </Card>
   );
