03dd300f7b850c0b63a403e10519ff0576fe5638
David Korpos <korposdavid@gmail.com>
2020-02-12 15:53:35 +0100

    separate players based on position

--- src/components/PlayerListContainer.tsx
+++ src/components/PlayerListContainer.tsx
@@ -5,6 +5,7 @@ import Col from "react-bootstrap/Col";
 import PlayerList from "./PlayerList";
 import PitchContainer from "./PitchContainer";
 import axios from "axios";
+import Player from "../models/Player";
 
 const PlayerListContainer = () => {
   const [playerList, setPlayerList] = useState([]);
@@ -20,16 +21,34 @@ const PlayerListContainer = () => {
     });
   };
 
+  const filterPlayersByPosition = (position: number) => {
+    return playerList.filter(function(player: Player) {
+      return player.element_type == position;
+    });
+  };
+
   useEffect(() => {
     fetchPlayers();
   }, []);
 
   return (
     <div>
-      <PlayerList players={playerList} title={"Goalkeepers"}></PlayerList>
-      <PlayerList players={playerList} title={"Defenders"}></PlayerList>
-      <PlayerList players={playerList} title={"Midfielders"}></PlayerList>
-      <PlayerList players={playerList} title={"Forwards"}></PlayerList>
+      <PlayerList
+        players={filterPlayersByPosition(1)}
+        title={"Goalkeepers"}
+      ></PlayerList>
+      <PlayerList
+        players={filterPlayersByPosition(2)}
+        title={"Defenders"}
+      ></PlayerList>
+      <PlayerList
+        players={filterPlayersByPosition(3)}
+        title={"Midfielders"}
+      ></PlayerList>
+      <PlayerList
+        players={filterPlayersByPosition(4)}
+        title={"Forwards"}
+      ></PlayerList>
     </div>
   );
 };
