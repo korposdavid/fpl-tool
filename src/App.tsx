5bdad9724491beb1a7668a06ea04acb261b66757
David Korpos <korposdavid@gmail.com>
2020-02-18 13:32:08 +0100

    implement AuthenticationProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./layout/NavBar";
-import PlayerListContainer from "./components/PlayerListContainer";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
 import AuthenticationProvider from "./components/AuthenticationProvider";
 import { CookiesProvider } from "react-cookie";
+import CustomRouter from "./components/CustomRouter";
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
-        <CookiesProvider>
-          <AuthenticationProvider>
-            <NavBar />
-            <PlayerListContainer />
-          </AuthenticationProvider>
-        </CookiesProvider>
-      </BrowserRouter>
+      <CookiesProvider>
+        <AuthenticationProvider>
+          <NavBar />
+          <CustomRouter />
+        </AuthenticationProvider>
+      </CookiesProvider>
     </div>
   );
 };
--- src/components/AuthenticationProvider.tsx
+++ src/components/AuthenticationProvider.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useContext, useMemo, createContext } from "react";
+import React, { useState, useContext, useEffect, createContext } from "react";
 import UserModel from "../models/UserModel";
+import { useCookies } from "react-cookie";
 
 interface Props {
   [x: string]: any;
@@ -7,23 +8,66 @@ interface Props {
 
 interface AuthContextInterface {
   user: UserModel | null;
-  login: (user: UserModel) => void;
+  login: () => void;
   logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextInterface>({
   user: null,
-  login: (user: UserModel) => {},
+  login: () => {},
   logout: () => {}
 });
 
 const AuthenticationProvider: React.FC<Props> = props => {
   const [user, setUser] = useState<UserModel | null>(null);
+  const [cookies] = useCookies(["XSRF-TOKEN"]);
+
+  useEffect(() => {
+    console.log("useEffect runs in authprovider");
+    fetch("http://localhost:8080/api/user", { credentials: "include" }).then(
+      response => {
+        response.text().then(body => {
+          if (body === "") {
+            setUser(null);
+          } else {
+            setUser(JSON.parse(body));
+          }
+        });
+      }
+    );
+  }, []);
+
+  const loginFunc = () => {
+    console.log("loginfunc runs in authprovider");
+    let port = window.location.port ? ":" + window.location.port : "";
+    if (port === ":3000") {
+      port = ":8080";
+    }
+    window.location.href = "//" + window.location.hostname + port + "/private";
+  };
+
+  const logoutFunc = () => {
+    fetch("http://localhost:8080/api/logout", {
+      method: "POST",
+      credentials: "include",
+      headers: { "X-XSRF-TOKEN": cookies.verbose }
+    })
+      .then(res => res.json())
+      .then(response => {
+        window.location.href =
+          response.logoutUrl +
+          "?id_token_hint=" +
+          response.idToken +
+          "&post_logout_redirect_uri=" +
+          window.location.origin;
+      });
+    setUser(null);
+  };
 
   const auth = {
     user,
-    login: (user: UserModel) => setUser(user),
-    logout: () => setUser(null)
+    login: loginFunc,
+    logout: logoutFunc
   };
 
   return <AuthContext.Provider value={auth} {...props} />;
