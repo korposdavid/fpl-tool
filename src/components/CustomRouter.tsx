100793cd20dfcd5b30d273f15e4dfa515858b17c
David Korpos <korposdavid@gmail.com>
2020-02-18 13:31:17 +0100

    rename and refector Router.tsx to CustomRouter.tsx

---
+++ src/components/CustomRouter.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import PlayerListContainer from "./PlayerListContainer";
+import { useAuthentication } from "./AuthenticationProvider";
+import AuthPage from "./AuthPage";
+import { RouteProps } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+
+interface PrivateRouteProps extends RouteProps {
+  [x: string]: any;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = props => {
+  const { user } = useAuthentication();
+  const finalComponent = user ? props.component : AuthPage;
+
+  return <Route {...props} component={finalComponent} />;
+};
+
+const CustomRouter = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/home" component={AuthPage} />
+      <PrivateRoute path="/team" component={PlayerListContainer} />
+      <Redirect from="/" to="/home" />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default CustomRouter;
