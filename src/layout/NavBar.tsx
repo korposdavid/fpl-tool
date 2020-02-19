42f6a1fe1b033c94bca90e26be480156a5374ab5
David Korpos <korposdavid@gmail.com>
2020-02-19 14:28:04 +0100

    separate navbar to nonauth and auth version

--- src/layout/NavBar.tsx
+++ src/layout/NavBar.tsx
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import { useAuthentication } from "../components/AuthenticationProvider";
 
 const NavBar = () => {
+  const { user, login, logout } = useAuthentication();
+
+  const nonAuthContent = (
+    <Fragment>
+      <Button
+        onClick={() => login()}
+        variant="outline-primary"
+        style={{ marginLeft: "2rem" }}
+      >
+        Login
+      </Button>
+    </Fragment>
+  );
+
+  const authContent = (
+    <Fragment>
+      <Button
+        href="/create"
+        variant="outline-primary"
+        style={{ marginLeft: "2rem" }}
+      >
+        Create Team
+      </Button>
+      <Button
+        href="/team"
+        variant="outline-primary"
+        style={{ marginLeft: "2rem" }}
+      >
+        My Team
+      </Button>
+      <Button
+        onClick={() => logout()}
+        variant="outline-primary"
+        style={{ marginLeft: "2rem" }}
+      >
+        Logout
+      </Button>
+    </Fragment>
+  );
+
+  const navbarContent = user ? authContent : nonAuthContent;
+
   return (
     <div>
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">FPL Tools</Navbar.Brand>
+        {navbarContent}
       </Navbar>
     </div>
   );
