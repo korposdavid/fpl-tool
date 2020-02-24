bae121f662fa63cbbaee76418219dde6568777c6
David Korpos <korposdavid@gmail.com>
2020-02-24 14:23:09 +0100

    redirect user to homepage after clicking brand on navbar

--- src/layout/NavBar.tsx
+++ src/layout/NavBar.tsx
@@ -49,7 +49,7 @@ const NavBar = () => {
   return (
     <div>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">FPL Tools</Navbar.Brand>
+        <Navbar.Brand href="/home">FPL Tools</Navbar.Brand>
         {navbarContent}
       </Navbar>
     </div>
